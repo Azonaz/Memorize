f7b6e68c92c3ac3f2e28ad3c94f06f61f0d25beb
Azonaz <malinina.a@gmail.com>
2024-01-21 08:44:27 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Админ on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
