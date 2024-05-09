25c7e3489cab505527aaf658e2694f7b5d26e60e
Azonaz <malinina.a@gmail.com>
2024-05-09 16:17:14 +0300

    lecture 5-6

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -2,17 +2,6 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: MemorizeViewModel
-//    @State var emojis: [String] = []
-//    let emojisAnimal = ["🐱", "🐥", "🐙", "🐼", "🦩", "🐑", "🐭", "🐳", "🐍", "🦋"]
-//    let emojisFood = ["🥐", "🧁", "☕️", "🍕", "🍦", "🍜", "🍔", "🍗", "🍧", "🍳", "🥗"]
-//    let emojisTransport = ["🚁", "🚘", "✈️", "⛵️", "🚂", "🚜", "🛵", "🚚", "🚲", "🛥️"]
-    
-    @State var selectedCategory: EmojiCategory = .animal
-    @State var cardColor: Color = .orange
-    
-    enum EmojiCategory {
-        case animal, food, transport
-    }
     
     var body: some View {
         Text("Memorize!")
@@ -20,93 +9,27 @@ struct EmojiMemoryGameView: View {
         VStack {
             ScrollView {
                 cards
+                    .animation(.default, value: viewModel.cards)
             }
             Button("Shuffle") {
                 viewModel.shuffle()
             }
-            Spacer()
-            cardAdjusters
-        }
-        .padding()
-        .onAppear {
-            updateEmojis()
         }
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: viewModel.widthThatBestFits()), spacing: 0)], spacing: 0) {
-            ForEach(0..<viewModel.cards.count, id: \.self) { index in
-                CardView(viewModel.cards[index])
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 4)], spacing: 4) {
+            ForEach(viewModel.cards) { card in
+                CardView(card)
                     .aspectRatio(2/3, contentMode: .fit)
-                    .padding()
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
             }
         }
-        .foregroundColor(cardColor)
+        .padding(16)
+        .foregroundColor(.orange)
     }
-    
-    var cardAdjusters: some View {
-        HStack {
-            cardsAnimal
-            Spacer()
-            cardsFood
-            Spacer()
-            cardsTransport
-        }
-    }
-    
-    func cartAdjuster (symbol: String, text: String, category: EmojiCategory) -> some View {
-        Button(action: {
-            selectedCategory = category
-            updateEmojis()
-        }, label: {
-            VStack {
-                Image(systemName: symbol)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 40, height: 40)
-                    .padding(.bottom, 5)
-                Text(text)
-                    .font(.body)
-            }
-        })
-    }
-    
-    func updateEmojis() {
-        var combinedEmojis: [String] = []
-        var randomCount: Int = 4
-        
-        switch selectedCategory {
-        case .animal:
-            randomCount = Int.random(in: 4...viewModel.emojisAnimal.count)
-            let selectedEmojis = Array(viewModel.emojisAnimal.shuffled().prefix(randomCount))
-            combinedEmojis = selectedEmojis + selectedEmojis
-            cardColor = .orange
-        case .food:
-            randomCount = Int.random(in: 4...viewModel.emojisFood.count)
-            let selectedEmojis = Array(viewModel.emojisFood.shuffled().prefix(randomCount))
-            combinedEmojis = selectedEmojis + selectedEmojis
-            cardColor = .green
-        case .transport:
-            randomCount = Int.random(in: 4...viewModel.emojisTransport.count)
-            let selectedEmojis = Array(viewModel.emojisTransport.shuffled().prefix(randomCount))
-            combinedEmojis = selectedEmojis + selectedEmojis
-            cardColor = .red
-        }
-//        emojis = combinedEmojis.shuffled()
-    }
-    
-    var cardsAnimal: some View {
-        cartAdjuster(symbol: "pawprint", text: "Animals", category: .animal)
-    }
-    
-    var cardsFood: some View {
-        cartAdjuster(symbol: "mug", text: "Food", category: .food)
-    }
-    
-    var cardsTransport: some View {
-        cartAdjuster(symbol: "car", text: "Transport", category: .transport)
-    }
-    
 }
 
 struct CardView: View {
@@ -130,6 +53,7 @@ struct CardView: View {
             .opacity(card.isFaceUp ? 1 : 0)
             base.fill().opacity(card.isFaceUp ? 0 : 1)
         }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
--- Memorize/MemorizeModel.swift
+++ Memorize/MemorizeModel.swift
@@ -1,28 +1,60 @@
 import Foundation
 
-struct MemorizeModel<CardContent> {
+struct MemorizeModel<CardContent> where CardContent: Equatable {
     private(set) var cards: Array<Card>
+    
+    var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get {
+            cards.indices.filter { index in cards[index].isFaceUp }.only
+        }
+        set {
+            cards.indices.forEach { cards[$0].isFaceUp = (newValue == $0) }
+        }
+    }
     
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = []
         for pairIndex in 0..<max(2, numberOfPairsOfCards) {
             let content = cardContentFactory(pairIndex)
-            cards.append(Card(content: content))
-            cards.append(Card(content: content))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)a"))
+            cards.append(Card(content: content, id: "\(pairIndex + 1)b"))
         }
     }
     
     mutating func shuffle() {
         cards.shuffle()
+        print(cards)
     }
     
-    func choose(_ card: Card) {
-        
+    mutating func choose(_ card: Card) {
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}) {
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
+                if let potentialIndex = indexOfTheOneAndOnlyFaceUpCard {
+                    if cards[chosenIndex].content == cards[potentialIndex].content {
+                        cards[chosenIndex].isMatched = true
+                        cards[potentialIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
     }
     
-    struct Card {
-        var isFaceUp = true
+    struct Card: Equatable, Identifiable, CustomDebugStringConvertible {
+        var isFaceUp = false
         var isMatched = false
         let content: CardContent
+        var id: String
+        var debugDescription: String {
+            "\(id): \(content) \(isFaceUp ? "up" : "down") \(isMatched ? "matched" : "")"
+        }
+    }
+}
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
